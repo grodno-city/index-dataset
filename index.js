6e96360336b5dd3afec9807f96b8b5492ff01f71
Hanna Kazello <hanna.kazella@instinctools.ru>
2017-08-10 18:27:30 +0300

    indexFunds work correctly

--- index.js
+++ index.js
@@ -9,59 +9,47 @@ const client = new elasticsearch.Client({
 });
 
 var url = 'mongodb://'+localConfig.connection.host+'/'+localConfig.connection.name;
+const map = {};
+// const map ={
+//   'Книгохранение,фондир.,стандарт.': 1,
+// }
 
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server.");
   const collection = db.collection('raw-data-set');
-  // collection.find().toArray(function(err, docs) {
-  //     assert.equal(null, err);
-  //     docs.slice(0,1).forEach((doc) => {
-  //       console.log(doc.id);
-  //       indexReferences(doc, (err, resp) => {
-  //         if(err) console.log(err);
-  //       })
-  //     })
-  //
-  //     db.close();
-  //   });
-  indexReferences(null, (err, resp) => {
-    if(err) console.log(err);
-    else {
-      console.log(resp);
-    }
-  })
+  collection.find().toArray(function(err, docs) {
+      assert.equal(null, err);
+      docs.slice(0,100).forEach((doc) => {
+        if (doc.funds.length !== 0) {
+          doc.funds.map(fund => fund.name).forEach(expandMap);
+          indexReferences(doc, (err, resp) => {
+            if(err) {
+              console.log(doc.id, err.code);
+            }
+          });
+        }
+      });
+
+      db.close();
+      console.log(map);
+    });
 });
 
-const map ={
-  label: 'Книгохранение,фондир.,стандарт.',
-  tag: 1,
+function expandMap (fundsName) {
+  if (map[fundsName] === undefined) {
+    map[fundsName] = Object.keys(map).length + 1;
+  }
 }
 
 function indexReferences(record, callback){
-  // const body = [];
-  // record.funds.forEach((fund) => {
-  //   body.push({ index:  { _index: 'funds', _type: fund.name, _id: record.id } });
-  //   body.push({ recordID: record.id, count: fund.count });
-  // });
-  const body =[ { index:
-     { _index: 'funds',
-       _type: 1,
-       _id: 231 } },
-  { recordID: 231, count: 1 } ];
+  const body = [];
+  record.funds.forEach((fund) => {
+    body.push({ index:  { _index: 'funds', _type: map[fund.name], _id: record.id } });
+    body.push({ recordID: record.id, count: fund.count });
+  });
 
   client.bulk({
   body
 }, callback);
-}
-
-function indexRecord(record, callback) {
-  client.index({
-    index: 'references',
-    type: record.references,
-    id: record.id,
-    body: {
-      record,
-    },
-  }, callback);
 }
