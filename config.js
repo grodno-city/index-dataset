0573026139473ab6e1a35156391568699821bd80
Hanna Kazello <hanna.kazella@instinctools.ru>
2017-08-14 09:46:20 +0300

    use works local confog

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ module.exports = {
     connection: {
         host: "localhost",
         port: 27017,
-        name: "test"
+        name: "library"
     },
     application: {
         port: 8080,
         DEBUG: true
     },
-    "elasticHost": "https://elastic:jesEsUy5exSj0LpQVBo1TnPY@7628f5bab434a3d85d1b9726b8576a21.eu-west-1.aws.found.io",
-    "elasticPort": 9243,
+    "elasticHost": "127.0.0.1",
+    "elasticPort": 9200,
 }
