9c55c616a0dbce09a606651c10d99957e5331a92
Hanna Kazello <hanna.kazella@instinctools.ru>
2017-09-21 17:27:11 +0300

    move processItem to another file, rewrite indexItem

--- index.js
+++ index.js
@@ -1,91 +1,44 @@
 import assert from 'assert';
 import { MongoClient } from 'mongodb';
 import elasticsearch from 'elasticsearch';
-import fs from 'fs';
 
-import fundsMap from './fundsMap.json';
 import localConfig from './config';
-import fieldsMap from './fieldsMap.json';
+import processItem from './processItem';
 
 const client = new elasticsearch.Client({
-  host: `${localConfig.elasticHost}:${localConfig.elasticPort}`,
+  host: `http://${localConfig.elasticHost}:${localConfig.elasticPort}`,
+  requestTimeout: 300000,
   // log: 'trace',
 });
 
-function indexFunds(body, record) {
-  record.funds.forEach((fund) => {
-    if (fundsMap[fund.name] === undefined) {
-      fundsMap[fund.name] = Object.keys(fundsMap).length + 1;
-    }
-    body.push({ index:  { _index: 'funds', _type: fundsMap[fund.name], _id: record.id } });
-    body.push({ recordID: record.id, count: fund.count });
-  });
-  return body;
-}
-
-function indexReferences(body, record) {
-  record.references.forEach((ref) => {
-    const values = ref.value.split('*');
-    values.forEach((val) => {
-      body.push({ index:  { _index: 'references', _type: ref.tag, _id: record.id } });
-      body.push({ recordID: record.id, value: val });
-    });
-  });
-  return body;
-}
-
-function indexFields(body, record) {
-  record.fields.forEach((field) => {
-    if (fieldsMap[field.tag] === undefined) {
-      fieldsMap[field.tag] = Object.keys(fieldsMap).length + 1;
-    }
-    body.push({ index:  { _index: 'fields', _type: fieldsMap[field.tag], _id: record.id } });
-    body.push({ recordID: record.id, value: field.value });
-  });
-  return body;
-}
-
-
-function indexRecord(record, callback) {
-  const body = [
-    { index:  { _index: 'raw', _type: 'def', _id: record.id } },
-    { record },
-  ].concat(indexFields([], record))
-  .concat(indexFunds([], record))
-  .concat(indexReferences([], record));
-  callback(body);
-}
-
-
 const url = `mongodb://${localConfig.connection.host}/${localConfig.connection.name}`;
 
-MongoClient.connect(url, (connectionErr, db) => {
-  assert.equal(null, connectionErr);
-  console.log('Connected correctly to server.');
-  const collection = db.collection('books');
-  collection.find((err, cursor) => {
-    cursor.each((err, item) => {
+MongoClient.connect(
+  url,
+  (connectionErr, db) => {
+    assert.equal(null, connectionErr);
+    console.log('Connected correctly to server.');
 
-      if (item != null) {
-        indexRecord(item, (body) => {
-          if (body.length !== 0) {
-            client.bulk({
-              body,
-            }, (indexErr) => {
-              if (indexErr) {
-                console.log(item.id, indexErr.message);
-              } else {
-                console.log(item.id);
-              }
-            });
-          }
+    const collection = db.collection('books');
+
+    collection.count().then((count) => {
+      console.log('Count of Records: ', count);
+    });
+
+    collection.find({}, (findErr, resultCursor) => {
+      function indexItem(err, item) {
+        if (item === null) {
+          return; // All done!
+        }
+        processItem(item, (processItemErr, body) => {
+          client.bulk({ body }, (bulkErr) => {
+            if (bulkErr) {
+              db.connection.close();
+            }
+            resultCursor.nextObject(indexItem);
+          });
         });
-      } else {
-        console.log("That's all!");
-        db.close();
-        fs.writeFileSync('fundsMap.json', JSON.stringify(fundsMap, null, 2));
-        fs.writeFileSync('fieldsMap.json', JSON.stringify(fieldsMap, null, 2));
       }
+      resultCursor.nextObject(indexItem);
     });
   });
-});
