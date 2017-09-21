e7448fe90fd1d0ed224f6aa4a746b6feec71c581
Hanna Kazello <hanna.kazella@instinctools.ru>
2017-09-21 17:25:12 +0300

    use local elastic

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    connection: {
-        host: "localhost",
-        port: 27017,
-        name: "library"
-    },
-    application: {
-        port: 8080,
-        DEBUG: true
-    },
-    "elasticHost": "127.0.0.1",
-    "elasticPort": 9200,
-}
+  connection: {
+    host: 'localhost',
+    port: 27017,
+    name: 'library',
+  },
+  application: {
+    port: 8080,
+    DEBUG: true,
+  },
+  elasticHost: 'localhost',
+  elasticPort: 9200,
+};
